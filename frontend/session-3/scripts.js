5f576b1432892de84d51d849ace1775fbaafc62e
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2018-11-15 23:59:00 +0200

    Scroll task done

--- frontend/session-3/scripts.js
+++ frontend/session-3/scripts.js
@@ -17,23 +17,40 @@ audioElement.onplaying = function() { isPlaying = true };
 audioElement.onpause = function() { isPlaying = false };
 
 //Task 2
-const sections = document.querySelectorAll('section');
+function buildThreshholdList() {
+    var thresholds = [0];
+    var numSteps = 10;
 
-var isInViewport = function(elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >=0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    for(let i=1.0; i <= numSteps; i++) {
+        var ratio = i/numSteps; // Btw, what's the difference between let and var?
+        thresholds.push(ratio);
+    }
+
+    console.log(thresholds);
+    return thresholds;
+}
+
+const intersectionThreshhold = 0.9;
+
+function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+        var section = entry.target;
+        if (entry.intersectionRatio > intersectionThreshhold) {
+            section.classList.add('section-bg-change');
+        } else {
+            section.classList.remove('section-bg-change');
+        }
+    })
+}
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: buildThreshholdList()
 };
 
-sections.forEach(function(section) {
-    section.addEventListener('scroll', function(event) {
-        console.log(event);
-        if (isInViewport(section)) {
-            section.classList.toggle('section-bg-change');
-        }
-    });
+const observer = new IntersectionObserver(handleIntersect, options);
+const sections = document.querySelectorAll('section');
+sections.forEach(section => {
+    observer.observe(section);
 });
--- frontend/session-3/tasks.md
+++ frontend/session-3/tasks.md
@@ -1,5 +1,6 @@
 [x] clicking on the image from the header toggles the audio component to play/pause
    
-[ ] when scrolling, if a section is in the middle of the screen, change its background, when it leaves the middle of the screen, reset it to its original value
+[x] when scrolling, if a section is in the middle of the screen, change its background, when it leaves the middle of the screen, reset it to its original value
   
-[ ] add a button in the aside element that when clicked inserts a new article in the main element, at the top, inside the newly created article there should be a button, which when clicked adds a new section above itself, the section has 400 random alphabet characters. 
+// Suggestion: Intersection Observer API  
+[ ] add a button in the aside element that when clicked inserts a new article in the main element, at the top, inside the newly created article there should be a button, which when clicked adds a new section above itself, the section has 400 random alphabet characters.  
