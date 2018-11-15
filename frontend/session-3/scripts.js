4d16b365906fe479eb3c2240d1470f687e089a84
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2018-11-15 15:53:56 +0200

    scroll event not triggering...

--- frontend/session-3/scripts.js
+++ frontend/session-3/scripts.js
@@ -15,3 +15,25 @@ var isPlaying = false;
 
 audioElement.onplaying = function() { isPlaying = true };
 audioElement.onpause = function() { isPlaying = false };
+
+//Task 2
+const sections = document.querySelectorAll('section');
+
+var isInViewport = function(elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >=0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+sections.forEach(function(section) {
+    section.addEventListener('scroll', function(event) {
+        console.log(event);
+        if (isInViewport(section)) {
+            section.classList.toggle('section-bg-change');
+        }
+    });
+});
