cc6f8e331cec87144ff8ce019e801ff05138edfd
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2018-11-22 15:47:11 +0200

    progress on Java 8 Assignment

--- backend/session-4/src/main/java/com/ing/devschool/Java8Assignment.java
+++ backend/session-4/src/main/java/com/ing/devschool/Java8Assignment.java
@@ -2,106 +2,137 @@ package com.ing.devschool;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Java8Assignment {
 
-    public static void main(String[] args) {
-        /**
-         * Given the following input data.
-         */
-        List<Integer> numbers = IntStream.range(0, 1_000)
-                .boxed()
-                .collect(Collectors.toList());
+	public static void main(String[] args) {
+		/*
+		 * Given the following input data.
+		 */
+		List<Integer> numbers = IntStream.range(0, 1_000)
+				.boxed()
+				.collect(Collectors.toList());
 
-        List<String> words = Arrays.asList("One", "Two", "three", "four", "five", "six", "seven", "Eight", "nine",
-                "ten", "eleven", "twelve", "thirteen", "Fourteen", "fifteen", "sixteen", "Nineteen", "twenty");
+		List<String> words = Arrays.asList("One", "Two", "three", "four", "five", "six", "seven", "Eight", "nine",
+				"ten", "eleven", "twelve", "thirteen", "Fourteen", "fifteen", "sixteen", "Nineteen", "twenty");
 
-        /**
-         * Implement ONE method PER exercise given todos:
-         */
+		/*
+		 * Implement ONE method PER exercise given todos:
+		 */
 
-        /**
-         * For each
-         */
-        // 1: using "numbers" as input, print first ten numbers
-//        example(numbers);
-        // 2: using "numbers" as input print first ten odd numbers
-        // 3: using "numbers" as input print first ten even numbers
-        // 4: implement 1, 2, 3 using one method
+		/*
+		 * For each
+		 */
+		// 1: using "numbers" as input, print first ten numbers
+//        example(numbers)
+		numbers.stream().limit(10).forEach(e -> System.out.print(e + " "));
+		/*
+		 * another version, but requiring to call println over the result
+		 * String result = numbers.stream().limit(10).map(Object::toString).reduce("", (acc, e) -> acc+e+" ");
+		 * System.out.println(result);
+		 */
+		printSeparator();
 
-        /**
-         * Sums
-         */
-        // 5. using "numbers" as input print their sum
-        // 6. using "numbers" as input print the sum of the odd numbers
-        // 7. using "numbers" as input print the sum of the even numbers
-        // 8. using "numbers" as input print the sum of the even numbers which contain '7'
-        // 9. implement 5, 6, 7, 8 using one method
+		// 2: using "numbers" as input print first ten odd numbers
+		numbers.stream().filter(n -> n % 2 != 0).limit(10).forEach(e -> System.out.print(e + " "));
+		printSeparator();
 
-        /**
-         * Mixed map/filter/reduce
-         */
-        // 10. using "numbers" calculate the sum of the double of even numbers
-        // 11. using "numbers" calculate the sum of the triple of odd numbers
-        // 12. using "numbers" calculate the sum of their half (1/2)
-        // 13 implement 10, 11, 12 using one method
+		// 3: using "numbers" as input print first ten even numbers
+		numbers.stream().filter(n -> n % 2 == 0).limit(10).forEach(e -> System.out.print(e + " "));
+		printSeparator();
 
-        /**
-         * Collectors
-         */
-        // 14. using "words" as input print a string with each UPPERCASED word separated by ', '
-        // 15. using "words" as input create a List containing all words starting with a vowel
-        // 16. using "words" as input create a List containing all words ending in 'teen'
-        // 17. implement 11 and 12 using one method
-        // 18. using words as input create a Set of words with an odd number of letters
-        // 19. using words as input create a Set containing the number of letters in a word
+		// 4: implement 1, 2, 3 using one method
+		printFirstFiltered(numbers, 10, (n -> true));
+		printFirstFiltered(numbers, 10, (n -> n % 2 != 0));
+		printFirstFiltered(numbers, 10, (n -> n % 2 == 0));
+		System.out.println("End of foreach stuff\n\n");
+		/*
+		 * Sums
+		 */
+		// 5. using "numbers" as input print their sum
+		System.out.println(numbers.stream().reduce(0, (acc, e) -> acc+e));
+		// 6. using "numbers" as input print the sum of the odd numbers
+		System.out.println(numbers.stream().filter(n -> n % 2 != 0).reduce(0, (acc, e) -> acc + e));
+		// 7. using "numbers" as input print the sum of the even numbers
+		System.out.println(numbers.stream().filter(n -> n % 2 == 0).reduce(0, (acc, e) -> acc + e));
+		// 8. using "numbers" as input print the sum of the even numbers which contain '7'
+		System.out.println(numbers.stream().filter(n -> n.toString().contains("7")).filter(n -> n %2 == 0).reduce(0, (acc, e) -> acc + e));
+		// 9. implement 5, 6, 7, 8 using one method
 
-        /**
-         * Converting to java 8
-         */
-        // 20. convert the following code to java 8
-        nonJava8Method(numbers);
+		/*
+		 * Mixed map/filter/reduce
+		 */
+		// 10. using "numbers" calculate the sum of the double of even numbers
+		// 11. using "numbers" calculate the sum of the triple of odd numbers
+		// 12. using "numbers" calculate the sum of their half (1/2)
+		// 13 implement 10, 11, 12 using one method
 
-        /**
-         * Optional<T>
-         */
-        // 21. using "numbers" as input print the first odd number that can be divided by 121, if it does not exist print -1
-    }
+		/*
+		 * Collectors
+		 */
+		// 14. using "words" as input print a string with each UPPERCASED word separated by ', '
+		// 15. using "words" as input create a List containing all words starting with a vowel
+		// 16. using "words" as input create a List containing all words ending in 'teen'
+		// 17. implement 11 and 12 using one method
+		// 18. using words as input create a Set of words with an odd number of letters
+		// 19. using words as input create a Set containing the number of letters in a word
 
-    private static void example(List<Integer> numbers) {
-        numbers.stream()
-                .limit(10)
-                .forEach(System.out::print);
-    }
+		/*
+		 * Converting to java 8
+		 */
+		// 20. convert the following code to java 8
+		nonJava8Method(numbers);
 
-    private static void nonJava8Method(List<Integer> numbers) {
-        int sum = 0;
-        int counter = 0;
+		/*
+		 * Optional<T>
+		 */
+		// 21. using "numbers" as input print the first odd number that can be divided by 121, if it does not exist print -1
+	}
 
-        for (int i = 0; i < numbers.size(); i++) {
-            int num = 0;
+	private static <T> void printFirstFiltered(List<T> numbers, int count, Predicate<T> predicate) {
+		numbers.stream().filter(predicate).limit(count).forEach(e -> System.out.print(e + " "));
+		printSeparator();
+	}
 
-            if (numbers.get(i) % 2 == 0) {
-                num = numbers.get(i) + 5;
-            } else {
-                sum += 0;
-                continue;
-            }
+	private static void printSeparator() {
+		System.out.println("\n------------------------------\n");
+	}
 
-            num = num * 7;
-            if (num % 20 == 3) {
-                num = num / 20;
-            }
+	private static void example(List<Integer> numbers) {
+		numbers.stream()
+				.limit(10)
+				.forEach(System.out::print);
+	}
 
-            sum += (num / 10);
+	private static void nonJava8Method(List<Integer> numbers) {
+		int sum = 0;
+		int counter = 0;
 
-            if (++counter > 5) {
-                break;
-            }
-        }
+		for (int i = 0; i < numbers.size(); i++) {
+			int num = 0;
 
-        System.out.println(sum);
-    }
+			if (numbers.get(i) % 2 == 0) {
+				num = numbers.get(i) + 5;
+			} else {
+				sum += 0;
+				continue;
+			}
+
+			num = num * 7;
+			if (num % 20 == 3) {
+				num = num / 20;
+			}
+
+			sum += (num / 10);
+
+			if (++counter > 5) {
+				break;
+			}
+		}
+
+		System.out.println(sum);
+	}
 }
