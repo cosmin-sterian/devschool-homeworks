0b71fa1a0ef6c1d41dd20656a6b0f08756465a6f
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2018-11-29 17:31:07 +0200

    fixed xhr behaviour, must keep xhr reference in a wider scope

--- frontend/session-4.5/scripts/weatherParser.js
+++ frontend/session-4.5/scripts/weatherParser.js
@@ -1,15 +1,18 @@
+let xhr;
+let forecastBoolean;
 function requestDataFromWeatherAPI(event, forecast, inputData) {
     event.preventDefault();
-    console.log(event, forecast, inputData);
+    // console.log(event, forecast, inputData);
 
-    let xhr = new XMLHttpRequest();
+    xhr = new XMLHttpRequest();
+    forecastBoolean = forecast;
 
     if (!xhr) {
         alert("Can't create XMLHTTP instance");
         return false;
     }
 
-    xhr.onreadystatechange = fetchData(xhr);
+    xhr.onreadystatechange = fetchData;
     if (forecast) {
         xhr.open('GET', `https://www.metaweather.com/api/location/${inputData}`);
     } else {
@@ -18,23 +21,27 @@ function requestDataFromWeatherAPI(event, forecast, inputData) {
     xhr.send();
 }
 
-function fetchData(xhr) {
-    console.log(xhr);
-    console.log(xhr.status);
-    const forecastContainer = document.querySelector('#forecastContainer');
-    forecastContainer.style.display = "inline-block";
+function fetchData() {
+    // console.log(xhr);
+    // console.log(xhr.status);
     if (xhr.readyState === XMLHttpRequest.DONE) {
+        const forecastContainer = document.querySelector('#forecastContainer');
+        forecastContainer.style.display = "inline-block";
         forecastContainer.textContent = "";
-        console.log(JSON.parse(xhr.responseText));
+        // console.log(JSON.parse(xhr.responseText)[0]);
         if (xhr.status === 200) {
-            console.log('success');
-            forecastContainer.textContent = JSON.parse(xhr.responseText).message;
-            console.log(forecastContainer.textContent);
+            // console.log('success');
+            const response = JSON.parse(xhr.responseText);
+            if (forecastBoolean) {
+                forecastContainer.textContent = response.consolidated_weather[0].weather_state_name;
+            } else {
+                forecastContainer.textContent = response[0].woeid;
+            }
+            
+            // console.log(forecastContainer.textContent);
         } else {
             forecastContainer.textContent = 'Content not found';
         }
-    } else {
-        forecastContainer.textContent = 'xhr readyState other than DONE';
     }
 }
 
