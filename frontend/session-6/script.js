2ce4ac7b97e4320c0e598e88b36bab8f2f9042fc
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2018-12-06 00:48:06 +0200

    initial commit

---
+++ frontend/session-6/script.js
@@ -0,0 +1,18 @@
+class Cipher {
+    constructor(initialKey = 123) {
+        if (initialKey == null || isNaN(initialKey)) { // Sanity check
+            console.error(`Cipher initialKey = '${initialKey}' is not a Number!`);
+            return null;
+        }
+        this.initialKey = initialKey;
+        this.cipherLog = [] // List of strings representing the operations logs
+    }
+
+    encode(message) {
+        if (! typeof message === 'string') { // Sanity check
+            console.error("Please use a string message");
+            return null;
+        }
+
+    }
+}
