8e2990d3c4917890244d73ea1aa75348a7f43b46
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2018-12-06 15:37:05 +0200

    reverted to delay even when cached, removed cache access time test

--- frontend/session-6/scripts/script.js
+++ frontend/session-6/scripts/script.js
@@ -1,66 +1,19 @@
-import timeout from './timeout.js'
+import Cipher from './cipher.js'
 
-export default class Cipher {
-    constructor(initialKey = 123) {
-        if (initialKey == null || isNaN(initialKey)) { // Sanity check
-            console.error(`Cipher initialKey = '${initialKey}' is not a Number!`);
-            return null;
-        }
-        this.initialKey = initialKey;
-        this.cipherLog = [] // List of strings representing the operations logs
-        this.store = new Map();
-    }
+const cipherKey = document.querySelector("#cipherKey").textContent;
+const decoded = document.querySelector("#decoded").textContent || "DevSchool";
+const encoded = document.querySelector("#encoded");
+const logContaier = document.querySelector("#logContainer");
 
-    async encode(message) {
-        if (!(typeof message === 'string')) { // Sanity check
-            console.error("Please use a string message");
-            return null;
-        }
-        const encodedList = []
-        message.split('').forEach(chr => {
-            encodedList.push(chr.charCodeAt() * this.initialKey);
-        });
+let cipher;
+let lastKey;
 
-        const encoded = encodedList.join("-");
-        this.store.set(encoded, message);
-        await timeout(message.length * 100);
-        this.cipherLog
-            .push(`${new Date().toLocaleString()}: "${message}" encoded as "${encoded}"`); // LOG the operation
-        return encoded;
-    }
-
-    async decode(message) {
-        if (!(typeof message === 'string')) { // Sanity check
-            console.error("Please use a string message");
-            return null;
-        }
-
-        let decoded = "";
-        if (this.store.has(message)) { // Check if message(encoded) was cached before
-            decoded = this.store.get(message);
+function buttonListener(_) {
+    if (cipher == null) {
+        if (cipherKey.textContent == null) {
+            cipher = new Cipher();
         } else {
-            message.split("-").forEach(encodedChr => {
-                decoded += String.fromCharCode(encodedChr / this.initialKey);
-            });
-            await timeout(message.length * 100);
+            cipher = new Cipher(lastKey);
         }
-
-        this.cipherLog
-            .push(`${new Date().toLocaleString()}: "${message}" decoded as "${decoded}"`);
-        return decoded;
-    }
-
-    readLog() {
-        return this.cipherLog.join("\n");
     }
 }
-
-//TODO: Move test to a test script
-// (async () => {
-//     const cipher = new Cipher(20);
-//     const msg = await cipher.encode("DevSchool");
-//     console.log(msg);
-//     console.log(await cipher.decode(msg));
-//     console.log(cipher.readLog());
-//     console.log(await cipher.encode(123));
-// })();
--- frontend/session-6/test/test.spec.js
+++ frontend/session-6/test/test.spec.js
@@ -1,5 +1,5 @@
 import 'babel-polyfill'
-import Cipher from '../scripts/script.js'
+import Cipher from '../scripts/cipher.js'
 const { expect } = require("chai");
 
 describe("Cipher without specified key", function() {
@@ -8,57 +8,74 @@ describe("Cipher without specified key", function() {
     });
 });
 
+describe("Cipher with not a Number key", function() {
+    it("should return a null cipher", function() {
+        const fctWrapper = arg => { return function() { new Cipher(arg) } };
+        expect(fctWrapper("asd")).to.throw(Error);
+        expect(fctWrapper(NaN)).to.throw(Error);
+        expect(fctWrapper(null)).to.throw(Error);
+    });
+});
+
 describe("Cipher encode", function() {
-    const cipher = new Cipher(20); // Check if bad practice
     it("should return proper encoded text of a message", async function() {
         const message = "DevSchool";
         const expected = "1360-2020-2360-1660-1980-2080-2220-2220-2160"
-        // const cipher = new Cipher(20);
+        const cipher = new Cipher(20);
         expect(await cipher.encode(message)).to.be.equal(expected);
     });
 
-    it("should not leave the log list empty", function() {
+    it("should not leave the log list empty after an encode", async function() {
+        const message = "DevSchool";
+        const expected = "1360-2020-2360-1660-1980-2080-2220-2220-2160"
+        const cipher = new Cipher(20);
+        expect(await cipher.encode(message)).to.be.equal(expected);
         expect(cipher.readLog()).to.not.be.empty;
     });
 
     it("should be null if invalid message(not a string type) is passed", async function() {
+        const cipher = new Cipher(20);
         expect(await cipher.encode(123)).to.be.null;
     });
 });
 
 describe("Cipher decode", function() {
-    const cipher = new Cipher(20);
     it("should return proper decoded text of a message", async function() {
         this.timeout(10000); // Increase test timeout because of the 100ms/char timeout
         const message = "1360-2020-2360-1660-1980-2080-2220-2220-2160";
         const expected = "DevSchool";
+        const cipher = new Cipher(20);
         expect(await cipher.decode(message)).to.be.equal(expected);
     });
 
-    it("should not leave the log list empty", function() {
+    it("should not leave the log list empty", async function() {
+        this.timeout(10000); // Increase test timeout because of the 100ms/char timeout
+        const message = "1360-2020-2360-1660-1980-2080-2220-2220-2160";
+        const expected = "DevSchool";
+        const cipher = new Cipher(20);
+        expect(await cipher.decode(message)).to.be.equal(expected);
         expect(cipher.readLog()).to.not.be.empty;
     });
 });
 
 describe("Cipher cache", function () {
-    const cipher = new Cipher(20);
     it("should cache the message", async function() {
         const message = "DevSchool";
         const expected = "1360-2020-2360-1660-1980-2080-2220-2220-2160"
+        const cipher = new Cipher(20);
         await cipher.encode(message);
         expect(cipher.store.get(expected)).to.be.equal(message);
     });
+});
 
-    it("should decode the message in less than 0.5s", async function() {
-        const expected = "DevSchool";
-        const message = "1360-2020-2360-1660-1980-2080-2220-2220-2160"
-        const startTime = new Date().getTime();
-        expect(await cipher.decode(message)).to.be.equal(expected);
-        const endTime = new Date().getTime();
-        expect(endTime - startTime).to.be.lessThan(500);
-    });
-
-    it("should have 2 logs right now", function () {
+describe("Cipher log", function () {
+    it("should have 2 logs after 2 operations", async function() {
+        this.timeout(10000);
+        const message = "DevSchool";
+        const expected = "1360-2020-2360-1660-1980-2080-2220-2220-2160"
+        const cipher = new Cipher(20);
+        expect(await cipher.encode(message)).to.be.equal(expected);
+        expect(await cipher.decode(expected)).to.be.equal(message);
         expect(cipher.cipherLog.length).to.be.equal(2);
     });
 });
